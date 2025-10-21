bbefe800612b4b2ab6795ac602b071289f3c2ed7
sndp bag <sndpbagg@gmail.com>
2025-10-21 17:52:35 +0530

    updated middleware file

---
+++ src/Middleware/PermissionMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace sndpbag\DynamicRoles\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PermissionMiddleware
+{
+    public function handle(Request $request, Closure $next, ...$permissions)
+    {
+        if (!Auth::check()) {
+            abort(403, 'Unauthorized');
+        }
+
+        $user = Auth::user();
+
+        if (!method_exists($user, 'hasPermission')) {
+            abort(500, 'User model must use HasRolesAndPermissions trait');
+        }
+
+        if (!$user->hasAnyPermission($permissions)) {
+            abort(403, 'You do not have the required permission to access this resource.');
+        }
+
+        return $next($request);
+    }
+}
---
+++ src/Middleware/RoleMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace sndpbag\DynamicRoles\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if (!Auth::check()) {
+            abort(403, 'Unauthorized');
+        }
+
+        $user = Auth::user();
+
+        if (!method_exists($user, 'hasRole')) {
+            abort(500, 'User model must use HasRolesAndPermissions trait');
+        }
+
+        if (!$user->hasAnyRole($roles)) {
+            abort(403, 'You do not have the required role to access this resource.');
+        }
+
+        return $next($request);
+    }
+}
